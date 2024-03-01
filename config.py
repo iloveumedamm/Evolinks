b9067cde890e78c179cce42ccd9460768cb09f5d
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2024-03-01 22:51:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ SOURCE_CODE = os.environ.get(
     "SOURCE_CODE", "https://github.com/kevinnadar22/URL-Shortener-V2"
 )  # for upstream repo
 # image when someone hit /start
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "https://graph.org/file/8fa5b68cb072b77612aad.jpg")
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "")
 LINK_BYPASS = is_enabled(
     (os.environ.get("LINK_BYPASS", "1")), False
 )  # if true, urls will be bypassed
